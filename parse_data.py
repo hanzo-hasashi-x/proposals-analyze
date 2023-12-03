b5e845875f4feb025f65affad2707bef3f6d1a06
hanzo-hasashi-x <iusan7756@gmail.com>
2023-12-03 13:13:56 +0100

    v1.3 - added model saving and generation of text with using ChatGPT

--- parse_data.py
+++ parse_data.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 import requests
-import json
 
+from dotenv import load_dotenv
+import json
+import os
+
+load_dotenv()
+
+access_token = '' or os.getenv('FREELANCER_ACCESS_TOKEN') 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-    access_token = config['access_token']
     jobs = config['jobs']
 
 url = "https://www.freelancer.com/api/projects/0.1/projects/all"
@@ -15,7 +20,7 @@ headers = {'freelancer-oauth-v1': access_token}
 offset = 0
 
 projects = []
-for i in range(0,1500,100):
+for i in range(0,config["projects_num"],100):
 
     from_time, to_time = datetime(2020, 1, 1).timestamp(), datetime(2023,1,1).timestamp()
     data = {"jobs[]": jobs, "project_types[]": ['fixed'],# "from_time": int(from_time), "to_time": int(to_time),
@@ -23,11 +28,11 @@ for i in range(0,1500,100):
             "full_description": True, "offset": offset}#, "job_details": True}
     
     r = requests.request("GET", url, headers=headers, params=data)
-    print(r.url)
+    # print(r.url)
     
     data =  r.json()['result']
     
-    print(len(data['projects']))
+    # print(len(data['projects']))
     
     bids_url_template = 'https://www.freelancer.com/api/projects/0.1/projects/{}/bids/'
     for project in data['projects']:
@@ -44,7 +49,7 @@ for i in range(0,1500,100):
 
     offset += 100
 
-with open('data/machine_learning.json', 'w') as f:
+with open(f'data/{config["dataset_name"]}.json', 'w') as f:
     json.dump(projects, f)
 
 print(r.status_code)
---
+++ utils.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+
+import json
+
+def parse_project_bids(project):
+    base = [project['id'], project['title'], project['description'], project['budget']['minimum'],
+            project['budget']['maximum'], project['submitdate']]
+    
+    project_bids = []
+        
+    for bid in project['bids']:
+        row = [*base]
+        
+        reputation = bid['reputation']['entire_history']['category_ratings']
+        
+        row += [bid['score'], bid['highlighted'], bid['sealed'], bid['description'], bid['submitdate'], 
+                bid['amount'], bid['period'], reputation['quality'], reputation['communication'], 
+                reputation['professionalism'], bid['award_status'] == 'awarded']
+        
+        project_bids.append(row)
+    
+    return project_bids
+
+
+data_columns = ['project_id', 'project_title', 'project_description', 'project_min_amount',
+            'project_max_amount', 'project_time_submitted', 'bid_score', 'bid_highlighted', 'bid_sealed',
+            'bid_description', 'bid_time_submitted', 'bid_amount', 'bid_period', 'bid_quality', 
+            'bid_communication', 'bid_professionalism', 'bid_award_status']
+def load_bids_df(dataset_name):
+    with open(f'data/{dataset_name}.json', 'r') as f:
+        projects = json.load(f)
+
+    data = []
+    for project in projects:
+        project_bids = parse_project_bids(project)
+            
+        if [bid for bid in project_bids if bid[-1]]: # checking if there is awarded bid, if not skip
+            data+=project_bids
+
+    return pd.DataFrame(columns=data_columns, data=data)
+
+def num_transform(df):
+    df['project/bid_amount_proportion'] = df['bid_amount'] / df['project_min_amount']
+    df['bid_delay'] = (df['bid_time_submitted'] - df['project_time_submitted']) / (3600*24*7) 
+    
+    df = df.drop(['bid_amount', 'project_min_amount', 'project_max_amount', 
+                  'project_time_submitted', 'bid_time_submitted'], axis=1)
+    return df
+
+def get_proportion(df, column_name):
+    proportions = []
+
+    projects_titles_words = df[column_name].apply(lambda x: x.split()).apply(set)
+    bid_descrs_words = df['bid_description'].apply(lambda x: x.split()).apply(set)
+
+    for project_title_set, bid_descr_set in zip(projects_titles_words, bid_descrs_words):
+        words_checked = [word in bid_descr_set for word in project_title_set]
+        proportions.append(sum(words_checked) / len(words_checked))
+
+    return proportions
+
+greetings = ['good afternoon', 'good morning', 'how are you', 'how do you do', 'dear sir',
+             'good night', 'hi', 'hello', 'pleased to meet you']
+
+def transform_text_columns(df):
+    
+    df['bid_description'] = df['bid_description'].fillna('')
+
+    df['same_words_title/bid_proportion'] = get_proportion(df, 'project_title')
+    df['same_words_descr/bid_proportion'] = get_proportion(df, 'project_description')
+
+    df['bid_descr_len'] = df['bid_description'].apply(lambda x: len(x.split()))
+    
+    proj_descr_len = df['project_description'].apply(lambda x: len(x.split()))
+    df['proj/bid_lens_proportion'] = df['bid_descr_len'] / proj_descr_len
+
+    df['bid_greetings'] = df['bid_description'].apply(lambda x: x.lower()).apply(
+        lambda x: any([x.find(greeting) != -1  for greeting in greetings]))
+    
+    df = df.drop(['project_title', 'project_description', 'bid_description'], axis=1)
+    
+    return df
+
+def prepare_df(df):
+
+    df = df.copy()
+
+    df = num_transform(df)
+    df = transform_text_columns(df)
+
+    df = df.drop(['bid_highlighted'], axis=1)
+    df[df['bid_score'] > 50.] = np.mean(df['bid_score'])
+
+    return df
